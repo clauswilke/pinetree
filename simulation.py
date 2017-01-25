6ba12c5b8458130576106e66d5f30a6b5be17522
Benjamin Jack <benjamin.r.jack@gmail.com>
2017-01-25 15:21:30 -0600

    basic collision detection

--- simulation.py
+++ simulation.py
@@ -36,29 +36,44 @@ class Tracker:
 
     def __init__(self, polymer, polymerase):
         self.polymer = polymer
-        self.polymerases = [polymerase]
+        self.polymerases = []
         self.time = 0
-        self.heap = self.build_heap()
+        self.heap = []
+        self.occupancy = [0]*polymer.length
+        self.add_polymerase(polymerase)
 
-    def build_heap(self):
-        heap = []
-        for pol in self.polymerases:
-            heapq.heappush(heap, (self.calculate_time(pol), pol))
-        return heap
+    def add_polymerase(self, pol):
+        self.polymerases.append(pol)
+        for i in range(pol.start, pol.start + pol.footprint + 1):
+            self.occupancy[i] = 1
+        heapq.heappush(self.heap, (self.calculate_time(pol), pol))
 
     def calculate_time(self, pol):
         return self.time + random.expovariate(pol.speed)
 
     def execute(self):
         next_pol = heapq.heappop(self.heap)
-        next_pol[1].move()
+        if self.check_collision(next_pol[1]) == False:
+            self.occupancy[next_pol[1].start] = 0
+            self.occupancy[next_pol[1].start + next_pol[1].footprint + 1] = 1
+            next_pol[1].move()
         self.time = next_pol[0]
         heapq.heappush(self.heap,
                        (self.calculate_time(next_pol[1]), next_pol[1]))
 
+    def check_collision(self, pol):
+        if self.occupancy[pol.start + pol.footprint + 1] == 0:
+            return False
+        else:
+            return True
+
+
     def __str__(self):
-        out_string = "Polymerase position: " + str(self.polymerases[0].start)
-        out_string += ", time: " + str(self.time)
+        out_string = "Polymerase position: "
+        for pol in self.polymerases:
+            out_string += str(pol.start) + ", "
+        out_string += "time: " + str(self.time)
+        out_string += ", occupancy: \n" + ''.join(map(str, self.occupancy))
         return out_string
 
 
@@ -75,11 +90,13 @@ def main():
 
     # Construct polymerases
     rna_pol = Polymerase("rna_pol", 1, 10, 10)
+    rna_pol2 = Polymerase("rna_pol2", 40, 10, 1)
 
     # Construct Tracker
     tracker = Tracker(polymer, rna_pol)
+    tracker.add_polymerase(rna_pol2)
 
-    while(tracker.time < 100):
+    while(tracker.time < 20):
         tracker.execute()
         print(tracker)
 
