6147a04af150d2e7d84aa93df8079b04da9cec67
Benjamin Jack <benjamin.r.jack@gmail.com>
2017-03-10 10:05:04 -0600

    finished Simulation unit tests

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
 import unittest
+import random
 from unittest.mock import patch, MagicMock
 
 from pysinthe.simulation import (Simulation, Reaction, SpeciesReaction, Bind,
@@ -172,7 +173,7 @@ class TestBridgeMethods(unittest.TestCase):
         self.mock_signal.return_value.fire.assert_called_with(-1)
 
 
-class TestSimulation(unittest.TestCase):
+class TestSimulationMethods(unittest.TestCase):
 
     def setUp(self):
         self.sim = Simulation()
@@ -194,6 +195,20 @@ class TestSimulation(unittest.TestCase):
         reaction1.calculate_propensity.return_value = 0.1
         self.assertNotEqual(self.sim.alpha_list[0], 0.1)
 
+    def test_register_polymer(self):
+        self.setUp()
+        polymer = MagicMock()
+        mock_promoter = MagicMock()
+        mock_promoter.type = "promoter"
+        mock_promoter.name = "mypromoter"
+        polymer.elements = [mock_promoter]
+        self.sim.register_polymer(polymer)
+        # Make sure promoter/polymer was added to map
+        self.assertEqual(self.sim.tracker.polymers_with_promoter("mypromoter"),
+                         [polymer])
+        # Make sure bridge reaction was added
+        self.assertEqual(self.sim.reactions[0].polymer, polymer)
+
     def test_initialize_propensity(self):
         # add reactions to simulation as before
         self.setUp()
@@ -203,24 +218,42 @@ class TestSimulation(unittest.TestCase):
         # Update cache
         self.sim.initialize_propensity()
         self.assertEqual(self.sim.alpha_list[0], 0.1)
-
-    def test_initialize_propensity(self):
-        pass
+        # reaction2 propensity should not have changed
+        self.assertEqual(self.sim.alpha_list[1], 0.8)
 
     def test_update_propensity(self):
-        pass
+        # add reactions to simulation as before
+        self.setUp()
+        self.test_register_reaction()
+        # Make sure that cache has not been updated
+        self.assertNotEqual(self.sim.alpha_list[0], 0.1)
+        # Update cache
+        self.sim.update_propensity(0)
+        self.assertEqual(self.sim.alpha_list[0], 0.1)
+        # Update propensity in reaction object
+        self.sim.reactions[1].calculate_propensity.return_value = 7
+        self.assertNotEqual(self.sim.alpha_list[1], 7)
+        self.sim.update_propensity(1)
+        self.assertEqual(self.sim.alpha_list[1], 7)
 
     def test_execute(self):
-        pass
+        self.setUp()
+        reaction1 = MagicMock()
+        reaction1.calculate_propensity.return_value = 0.5
+        self.sim.register_reaction(reaction1)
+        reaction2 = MagicMock()
+        reaction2.calculate_propensity.return_value = 0.8
+        self.sim.register_reaction(reaction2)
 
-    def test_register_transcript(self):
-        pass
-
-    def test_terminate_transcription(self):
-        pass
-
-    def test_terminate_translation(self):
-        pass
-
-    def test_count_termination(self):
-        pass
+        self.sim.initialize_propensity()
+        random.seed(17)
+        old_iteration = self.sim.iteration
+        old_time = self.sim.time
+        new_time = old_time + 0.5000911660616504
+        self.sim.execute()
+        self.assertEqual(self.sim.time, new_time)
+        self.assertTrue(reaction2.execute.called)
+        self.assertEqual(self.sim.iteration, old_iteration + 1)
+        self.assertFalse(reaction1.execute.called)
+        self.sim.execute()
+        self.assertTrue(reaction1.execute.called)
