a34f79ba36dd3e52a315635341dd214af4bd1f3b
Benjamin Jack <benjamin.r.jack@gmail.com>
2017-03-21 15:30:45 -0500

    refactored parse_genbank to pull down standard T7 gene numbering

--- pysinthe/parse_genbank.py
+++ pysinthe/parse_genbank.py
@@ -9,45 +9,51 @@ Entrez.email = "benjamin.r.jack@gmail.com"
 # "Promoters" are really transcription start sites!!
 # Likewise, "terminators" are really the final position in a transcript
 
-# Download T7 wild-type and E. coli K12 genbank records
-handle = Entrez.efetch(db="nuccore", id=["NC_001604"], rettype="gb", retmode="text")
+# Download T7 wild-type genbank records
+handle = Entrez.efetch(db="nuccore",
+                       id=["NC_001604"],
+                       rettype="gb",
+                       retmode="text")
 
 records = SeqIO.parse(handle, "genbank")
 
 output = {"elements": []}
 
-length_sum = 0
-
 for record in records:
 
     for feature in record.features:
-        # Grab coding sequences
-        print(feature)
+        # print(feature)
         start = feature.location.start.position
         stop = feature.location.end.position
+        # Grab promoters and terminators
         if feature.type == "regulatory":
-            length = feature.location.end.position - feature.location.start.position
+            # Construct promoter params
             if "promoter" in feature.qualifiers["regulatory_class"]:
-                output["elements"].append({"type": "promoter",
-                                           "name": feature.qualifiers["note"][0],
-                                           "start": start,
-                                           "stop": stop,
-                                           "interactions": {"name": {"binding_constant": " "}}})
+                output["elements"].append({
+                     "type": "promoter",
+                     "name": feature.qualifiers["note"][0],
+                     "start": start,
+                     "stop": stop,
+                     "interactions": {"name": {"binding_constant": " "}}
+                })
+            # Construct terminator params
             if "terminator" in feature.qualifiers["regulatory_class"]:
-                output["elements"].append({"type": "terminator",
-                                           "name": feature.qualifiers["note"][0],
-                                            "start": start,
-                                            "stop": stop,
-                                           "interactions": {"name": {"efficiency": " "}}})
+                output["elements"].append({
+                    "type": "terminator",
+                    "name": feature.qualifiers["note"][0],
+                    "start": start,
+                    "stop": stop,
+                    "interactions": {"name": {"efficiency": " "}}
+                })
+        # Grab genes/CDSes
         if feature.type == "gene":
-            # Record some informationa about the sequence for the FASTA header
+            # Grab the gene name
             gene_name = feature.qualifiers["note"][0]
-
-
-            # Construct a string in FASTA format
+            # Construct CDS parameters for this gene
             output["elements"].append({"type": "transcript",
                                        "name": gene_name,
-                                        "start": start,
-                                        "stop": stop,
+                                       "start": start,
+                                       "stop": stop,
                                        "rbs": -15})
+
 print(dump(output, default_flow_style=False))
