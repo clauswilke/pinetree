f9e8969efb885dc559cda5d00555df06dfad97ac
Benjamin Jack <benjamin.r.jack@gmail.com>
2017-03-24 09:49:33 -0500

    added regression test for promoters that overlap with genes

--- tests/test_run_simulation.py
+++ tests/test_run_simulation.py
@@ -92,6 +92,18 @@ class MainTest(unittest.TestCase):
 
         self.assertEqual(results.getvalue(), text)
 
+    def test_gene_promoter_overlap(self):
+        stdout = sys.stdout  # keep a handle on the real standard output
+        results = io.StringIO()
+        sys.stdout = results  # Choose a file-like object to write to
+        simulation.main("tests/params/promoter_gene_overlap.yml")
+        sys.stdout = stdout
+
+        with open('tests/output/promoter_gene_overlap_out.csv') as f:
+            text = f.read()
+
+        self.assertEqual(results.getvalue(), text)
+
 
 if __name__ == '__main__':
     unittest.main()
