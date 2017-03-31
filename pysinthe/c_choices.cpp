3e794c5f3282d395311b7ec6f769ede5de7a58bd
Benjamin Jack <benjamin.r.jack@gmail.com>
2017-03-31 14:38:29 -0500

    rewrite weighted_choices in C

---
+++ pysinthe/c_choices.cpp
@@ -0,0 +1,32 @@
+<%
+setup_pybind11(cfg)
+%>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <algorithm>
+
+namespace py = pybind11;
+
+py::object weighted_choice(py::list population,
+                           const std::vector<double> &weights) {
+    py::object randfunc = py::module::import("random").attr("random");
+    double random_num = randfunc().cast<double>();
+    double sum = 0;
+    std::vector<double> cum_weights = std::vector<double>(weights.size());
+    for (std::vector<double>::size_type i = 0; i < cum_weights.size(); i++) {
+      sum = weights[i] + sum;
+      cum_weights[i] = sum;
+    }
+    std::vector<double>::iterator upper;
+    upper = std::upper_bound(cum_weights.begin(),
+                             cum_weights.end(),
+                             random_num * sum);
+    int index = (upper - cum_weights.begin());
+    return population[index];
+}
+
+PYBIND11_PLUGIN(c_choices) {
+    py::module m("c_choices", "weighted_choice rewritten in C++");
+    m.def("weighted_choice", &weighted_choice);
+    return m.ptr();
+}
