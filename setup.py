da1219b5742dc53f71773d609b1472f06c278958
Benjamin Jack <benjamin.r.jack@gmail.com>
2017-08-16 01:11:53 -0500

    fixed path issues with test executable

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ class CMakeBuild(build_ext):
             os.makedirs(self.build_temp)
         subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
         subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+        subprocess.check_call(['mv',self.build_temp + '/pinetree_test','./build/'])
 
 class CatchTestCommand(TestCommand):
 
