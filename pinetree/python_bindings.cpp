9e926311e2671f8c8b2bdc631b9fb0d013e0aae8
Benjamin Jack <benjamin.r.jack@gmail.com>
2018-01-22 16:51:50 -0600

    removed deprecated python bindings

--- pinetree/python_bindings.cpp
+++ pinetree/python_bindings.cpp
@@ -27,14 +27,6 @@ PYBIND11_MODULE(pinetree, m) {
       m, "Simulation", "Set up and run a gene expression simulation.")
       .def(py::init<int, int, double>(), "run_time"_a, "time_step"_a,
            "cell_volume"_a)
-      .def_property("stop_time",
-                    (double (Simulation::*)()) & Simulation::stop_time,
-                    (void (Simulation::*)(double)) & Simulation::stop_time,
-                    "stop time of simulation")
-      .def_property("time_step",
-                    (double (Simulation::*)()) & Simulation::time_step,
-                    (void (Simulation::*)(double)) & Simulation::time_step,
-                    "time step at which to output data")
       .def("add_reaction", &Simulation::AddReaction,
            "add a species-level reaction")
       .def("register_genome", &Simulation::RegisterGenome, "register a genome")
@@ -42,45 +34,6 @@ PYBIND11_MODULE(pinetree, m) {
       .def("add_polymerase", &Simulation::AddPolymerase, "name"_a,
            "footprint"_a, "speed"_a, "copy_number"_a, "add a polymerase")
       .def("run", &Simulation::Run, "run the simulation");
-
-  // Binding for abtract Reaction so pybind11 doesn't complain when doing
-  // conversions between Reaction and its child classes
-  py::class_<Reaction, Reaction::Ptr>(
-      m, "Reaction", "A generic Reaction designed to be extended.");
-  py::class_<SpeciesReaction, Reaction, SpeciesReaction::Ptr>(
-      m, "SpeciesReaction", "Define a species-level reaction.")
-      .def(py::init<double, double, const std::vector<std::string> &,
-                    const std::vector<std::string> &>());
-  py::class_<Bind, Reaction, std::shared_ptr<Bind>>(m, "Bind").def(
-      py::init<double, double, const std::string &, const Polymerase &>());
-  py::class_<Bridge, Reaction, std::shared_ptr<Bridge>>(m, "Bridge")
-      .def(py::init<Polymer::Ptr>());
-
-  // Features and elements
-  py::class_<Feature, std::shared_ptr<Feature>>(m, "Feature")
-      .def_property("start", (int (Feature::*)()) & Feature::start,
-                    (void (Feature::*)(int)) & Feature::start)
-      .def_property("stop", (int (Feature::*)()) & Feature::stop,
-                    (void (Feature::*)(int)) & Feature::stop)
-      .def_property_readonly(
-          "type", (const std::string &(Feature::*)() const) & Feature::type)
-      .def_property_readonly(
-          "name", (const std::string &(Feature::*)() const) & Feature::name);
-  py::class_<Element, Feature, Element::Ptr>(m, "Element")
-      .def_property("gene",
-                    (const std::string &(Element::*)() const) & Element::gene,
-                    (void (Element::*)(const std::string &)) & Element::gene);
-  py::class_<Promoter, Element, Promoter::Ptr>(m, "Promoter")
-      .def(py::init<const std::string &, int, int,
-                    const std::map<std::string, double> &>());
-  py::class_<Terminator, Element, Terminator::Ptr>(m, "Terminator")
-      .def(py::init<const std::string &, int, int,
-                    const std::map<std::string, double> &>())
-      .def_property(
-          "reading_frame", (int (Terminator::*)()) & Terminator::reading_frame,
-          (void (Terminator::*)(int)) & Terminator::set_reading_frame);
-  py::class_<Polymerase, Feature, Polymerase::Ptr>(m, "Polymerase")
-      .def(py::init<const std::string &, int, int>());
 
   // Polymers, genomes, and transcripts
   py::class_<Polymer, Polymer::Ptr>(m, "Polymer");
