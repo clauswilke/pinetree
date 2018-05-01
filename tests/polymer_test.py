bde53dcfeb0b4ea7b046f4dd3be7e385c17c00a7
Benjamin Jack <benjamin.r.jack@gmail.com>
2018-04-30 19:15:26 -0500

    outlined MobileElementManager tests

---
+++ tests/polymer_test.py
@@ -0,0 +1,29 @@
+import unittest
+import pinetree as pt
+
+
+class TestMobileElementManagerMethods(unittest.TestCase):
+
+    def test_init(self):
+        manager = pt.MobileElementManager([])
+
+    def test_insert(self):
+        pass
+
+    def test_delete(self):
+        pass
+
+    def test_choose(self):
+        pass
+
+    def test_valid_index(self):
+        pass
+
+    def test_get_pol(self):
+        pass
+
+    def test_get_attached(self):
+        pass
+
+    def test_update_propensity(self):
+        pass
