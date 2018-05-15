e850b58fd3d05856d4165426ca84f790a4ef27f5
Benjamin Jack <benjamin.r.jack@gmail.com>
2018-05-15 14:49:59 -0500

    updated setup.py to point to correct download url

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ setup(
     author='Benjamin Jack',
     author_email='benjamin.r.jack@gmail.com',
     url='https://github.com/benjaminjack/pinetree',
-    download_url='https://github.com/benjaminjack/pinetree/archive/0.1.0.tar.gz'
+    download_url='https://github.com/benjaminjack/pinetree/archive/v0.1.0.tar.gz'
     license=license,
     keywords=['gene expression', 'gene', 'codon',
               'transcription', 'translation', 'biology', 'stochastic'],
