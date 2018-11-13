72cc15b9654f831f033a70969d93a08d67114174
Benjamin Jack <benjamin.r.jack@gmail.com>
2018-11-12 19:53:14 -0600

    skbuild is now used to build pinetree

--- setup.py
+++ setup.py
@@ -1,74 +1,7 @@
 #! /usr/bin/env python3
 
-import os
-import re
-import sys
-import sysconfig
-import platform
-import subprocess
-
-from distutils.version import LooseVersion
-from setuptools import setup, Extension, find_packages
-from setuptools.command.build_ext import build_ext
-from setuptools.command.test import test as TestCommand
-
-
-class CMakeExtension(Extension):
-    def __init__(self, name, sourcedir=''):
-        Extension.__init__(self, name, sources=[])
-        self.sourcedir = os.path.abspath(sourcedir)
-
-
-class CMakeBuild(build_ext):
-    def run(self):
-        try:
-            out = subprocess.check_output(['cmake', '--version'])
-        except OSError:
-            raise RuntimeError(
-                "CMake must be installed to build the following extensions: " +
-                ", ".join(e.name for e in self.extensions))
-
-        if platform.system() == "Windows":
-            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)',
-                                                   out.decode()).group(1))
-            if cmake_version < '3.1.0':
-                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
-
-        for ext in self.extensions:
-            self.build_extension(ext)
-
-    def build_extension(self, ext):
-        extdir = os.path.abspath(
-            os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
-                      '-DPYTHON_EXECUTABLE=' + sys.executable]
-
-        cfg = 'Debug' if self.debug else 'Release'
-        build_args = ['--config', cfg]
-
-        if platform.system() == "Windows":
-            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(
-                cfg.upper(),
-                extdir)]
-            if sys.maxsize > 2**32:
-                cmake_args += ['-A', 'x64']
-            build_args += ['--', '/m']
-        else:
-            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
-            build_args += ['--', '-j2']
-
-        env = os.environ.copy()
-        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
-            env.get('CXXFLAGS', ''),
-            self.distribution.get_version())
-        if not os.path.exists(self.build_temp):
-            os.makedirs(self.build_temp)
-        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args,
-                              cwd=self.build_temp, env=env)
-        subprocess.check_call(['cmake', '--build', '.'] + build_args,
-                              cwd=self.build_temp)
-        print()  # Add an empty line for cleaner output
-
+from skbuild import setup
+from setuptools import find_packages
 
 with open('README.md', encoding='utf-8') as f:
     readme = f.read()
@@ -76,7 +9,7 @@ with open('README.md', encoding='utf-8') as f:
 with open('LICENSE') as f:
     license = f.read()
 
-_VERSION = "0.1.3"
+_VERSION = "0.1.4"
 
 setup(
     name='pinetree',
@@ -93,8 +26,8 @@ setup(
               'translation', 'biology', 'stochastic'],
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    ext_modules=[CMakeExtension('pinetree/core')],
-    cmdclass=dict(build_ext=CMakeBuild),
     zip_safe=False,
-    test_suite='tests'
+    test_suite='tests',
+    cmake_args=['-DCMAKE_OSX_DEPLOYMENT_TARGET:STRING=10.9',
+                '-DCMAKE_OSX_ARCHITECTURES:STRING=x86_64']
 )
